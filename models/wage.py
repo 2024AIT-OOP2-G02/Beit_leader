52d0efec03e0ace3d27a1f51ae2aa9d74b55c650
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-01-09 15:39:49 +0900

    fix:typo

--- models/wage.py
+++ models/wage.py
@@ -4,7 +4,7 @@ from .db import db
 class Wage(Model):
     location = CharField() # 勤務地
     weekday_wage = FloatField() # 平日時給
-    holyday_wage = FloatField() # 休日時給
+    holiday_wage = FloatField() # 休日時給
     
 
     class Meta:
