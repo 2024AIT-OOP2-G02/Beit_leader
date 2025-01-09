9e7980d326cee5d100746626c91402e4d6ac0693
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-01-09 15:36:00 +0900

    fix:タイポ

--- models/shift.py
+++ models/shift.py
@@ -7,7 +7,7 @@ class Shift(Model):
     work_time = FloatField() # 勤務時間
     start_time = DateTimeField() # 勤務開始時間
     finish_time = DateTimeField() # 勤務終了時間
-    is_holyday = BooleanField(default=False) # 休日かどうか
+    is_holiday = BooleanField(default=False) # 休日かどうか
     
     class Meta:
         database = db
