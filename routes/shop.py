f1f5bcbf12c93300dea548412e6c1872a9167e48
piman528 <k23086kk@aitech.ac.jp>
2025-01-19 21:54:31 +0900

    uiの調整

--- routes/shop.py
+++ routes/shop.py
@@ -8,7 +8,7 @@ shop_bp = Blueprint('shop', __name__, url_prefix='/shop')
 @shop_bp.route('/')
 def list():
     wages = Wage.select()
-    return render_template('shop_list.html',shops = wages)
+    return render_template('shop_add.html',shops = wages)
     
 @shop_bp.route('/add', methods=['GET', 'POST'])
 def add():
@@ -17,6 +17,15 @@ def add():
             weekday_wage = request.form['weekday_wage']
             holiday_wage = request.form['holiday_wage']
             Wage.create(location=company_id,weekday_wage=weekday_wage,holiday_wage=holiday_wage)
-            return redirect(url_for("shop.add"))
+            return redirect(url_for("shop.list"))
         wages = Wage.select()
         return render_template('shop_add.html',shops = wages)
+   
+@shop_bp.route('/delete', methods=['POST']) 
+def delete():
+    if request.method == 'POST':
+        shop_name = request.form['shop_name']
+        Wage.delete().where(Wage.location == shop_name).execute()
+        return redirect(url_for("shop.list"))
+    wages = Wage.select()
+    return render_template('shop_add.html',shops = wages)
