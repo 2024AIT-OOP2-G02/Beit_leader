4ed8756c90c73e0d20b3d93a28361da2b3468e1d
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-01-22 13:09:34 +0900

    fix"ローカルストレージに格納する形式ではなくデータベースから取得した情報を表示するようにした"

--- routes/calendar.py
+++ routes/calendar.py
@@ -6,23 +6,25 @@ from datetime import datetime as dt
 # Blueprintの作成
 calendar_bp = Blueprint('calendar', __name__, url_prefix='/calendar')
 
-# @calendar_bp.route('/', methods=['GET'])
-# def list():
-#     shifts = Shift.select()
-#     wages = Wage.select()
-#     return render_template('calendar.html', shops=wages, shifts=shifts)
 @calendar_bp.route('/')
 def index():
     shops = Wage.select()
-    wages = Wage.select()
-    return render_template('calendar.html', shops=shops, shifts=wages)
+    shifts = Shift.select()
+    shifts_list = [{
+        'id': shift.id,
+        'start_time': shift.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+        'finish_time': shift.finish_time.strftime('%Y-%m-%d %H:%M:%S'),
+        'location': shift.wage.location,
+        'is_holiday': shift.is_holiday
+    } for shift in shifts]
+    return render_template('calendar.html', shops=shops, shifts=shifts_list)
 
 
 @calendar_bp.route('/add', methods=['GET', 'POST'])
 def add():
         
         if request.method == 'POST':
-            print(request.form)
+            print("入力した値",request.form)
             try:
                 shift_date = request.form['shift_date']
                 shift_in = request.form['shift_in']
@@ -30,7 +32,7 @@ def add():
                 location = request.form['location']
             except KeyError as e:
             # エラーハンドリング
-                return "Missing form data", 400
+                return redirect(url_for('calendar.add'))
 
             # 日付と時間を結合してdatetimeオブジェクトに変換
             start_time = dt.strptime(f"{shift_date} {shift_in}", '%Y-%m-%d %H:%M')
@@ -42,17 +44,36 @@ def add():
                 # もし該当するWageがなければ、新しいWageを作成
                 wage = Wage.create(location=location, weekday_wage=0, holiday_wage=0)
 
+            # 入力値のチェック
+            if start_time > finish_time:
+                return redirect(url_for('calendar.add'))
+            
+            work_time = (finish_time - start_time).total_seconds() / 3600
+            round(work_time,2)
+            
+            # 休日かどうかを判定
+            if(start_time.strftime('%a') == 'Sat' or start_time.strftime('%a') == 'Sun'):
+                is_holiday = True
+            else:
+                is_holiday = False
+            
             # Shiftテーブルに新しいシフトを追加
             Shift.create(
                 wage=wage,
-                work_time=(finish_time - start_time).total_seconds() / 3600,  # 勤務時間を計算
+                work_time=work_time,  # 勤務時間を計算
                 start_time=start_time,
                 finish_time=finish_time,
-                is_holiday=False  # 休日かどうかはデフォルトでFalse
+                is_holiday=is_holiday
             )
             return redirect(url_for('calendar.add'))
         
         else:
             shifts = Shift.select()
             wages = Wage.select()
-            return render_template('calendar.html', shops=wages, shifts=shifts)
+            shifts_list = [{
+            'start': shift.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'finish': shift.finish_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'title': shift.wage.location
+            } for shift in shifts]
+            print(shifts_list)
+            return render_template('calendar.html', shops=wages, shifts=shifts_list)
